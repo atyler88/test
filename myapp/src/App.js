18420f80a3595e55ec6f89d6124d1821d3bbe837
atyler88 <a.tyler88@gmail.com>
2018-09-29 18:24:23 -0500

    added props instead of hardcoding data

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
       <div className="App">
         <h1>My React App</h1>
         <p>welcome</p>
-        <Ninjas/>
+        <Ninjas name="ryu" age="25" belt="black"/>
+        <Ninjas name="yoshi" age="30" belt="green"/>
       </div>
     );
   }
--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 
 class Ninjas extends Component{
     render(){
+        //the next line is an example of destructuring the props we passed along from the parent component app.js
+        const {name, age, belt} = this.props;
         return(
             <div className="ninja">
-                <div>Name: Ryu</div>
-                <div>Age: 30</div>
-                <div>Belt: Black</div>
+
+                {/* Instead of writing props like this we can simplify them
+                 <div>Name: {this.props.name}</div>
+                <div>Age: {this.props.age}</div>
+                <div>Belt: {this.props.belt}</div> */}
+
+                <div>Name: {name}</div>
+                <div>Age: {age}</div>
+                <div>Belt: {belt}</div>
             </div>
         )
     }
